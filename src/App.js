47ac39aa6e274ce80bf82f2648cf6dba53daa9f5
Hashimjasem <j_hashim1305@outlook.com>
2023-09-18 18:22:31 +0800

    sudo code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import './App.css';
 
 function App() {  
   
-
+ 
   return (
-    <div>
+      /* background image*/
+      <div className='background'>
+      {/* two divs centered */}
+      {/* div1 */}
+      <div className='div1 container'>
+              {/* Location selecter */}
+              <p>Perth</p>
+              <p>sunny</p>
+      </div>
+
+      {/* div2 */}
+      <div className='div2 container'>
+        {/* temp */}
+        <p>32</p>
+      {/* wind speed */}
+        <p>21</p>
+      {/* uv */}
+        <p>8</p>
+      {/* rain */}
+        <p>0.6</p>
+      </div>
       
     </div>
   );
